d461a83966e8cddcba4a824062ad37ba75713a99
manojdeo <manojdeo@gmail.com>
2020-08-03 01:28:50 +0530

    Adding git

---
+++ src/main/java/com/manoj/lb/app/SpringbootGcpLbApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.lb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGcpLbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGcpLbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manoj/lb/app/SpringbootGcpLbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.lb.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGcpLbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
